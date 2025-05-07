d210540172377da080e109318b8ceb31acbb4ce8
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-05-07 22:12:35 +0200

    add enviroment

--- vars/sonarQubeAnalysisWithGate.groovy
+++ vars/sonarQubeAnalysisWithGate.groovy
@@ -18,31 +18,13 @@ def call(Map config = [:]) {
         echo "✅ SonarQube analysis completed for project '${projectKey}'."
 
         echo "⏳ Waiting Quality Gate..."
+        sleep(30)
         script {
-            def qg
-            def maxRetries = 10
-            def retryCount = 0
-            def delayBetweenChecks = 10
-
-            while (retryCount < maxRetries) {
-                qg = waitForQualityGate()
-
-                if (qg.status == 'OK') {
-                    echo "✅ SonarQube Quality Gate passed: ${qg.status}"
-                    break
-                } else if (qg.status == 'ERROR') {
-                    error("❌ SonarQube Quality Gate failed with status: ${qg.status}")
-                    break
-                } else if (qg.status == 'IN_PROGRESS') {
-                    echo "⚠️ Quality Gate status 'IN_PROGRESS'. Retrying in ${delayBetweenChecks} seconds..."
-                    retryCount++
-                    sleep(delayBetweenChecks)
-                } else {
-                    error("❌ Unknown Quality Gate status: ${qg.status}")
-                }
-            }
-            if (retryCount == maxRetries) {
-                error("❌ Maximum retries reached. Quality Gate is still IN_PROGRESS after ${maxRetries} attempts.")
+            def qg = waitForQualityGate()
+            if (qg.status != 'OK') {
+                error("❌ SonarQube Quality Gate failed with status: ${qg.status}")
+            } else {
+                echo "✅ SonarQube Quality Gate passed: ${qg.status}"
             }
         }
     } catch (err) {
