a2306e8c2ac678f31009de7ddf6f4d1d26fe7e29
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-05-07 22:28:49 +0200

    add enviroment

--- vars/sonarQubeAnalysisWithGate.groovy
+++ vars/sonarQubeAnalysisWithGate.groovy
@@ -18,17 +18,17 @@ def call(Map config = [:]) {
         echo "✅ SonarQube analysis completed for project '${projectKey}'."
 
         echo "⏳ Waiting Quality Gate..."
-        sleep(30)
-        script {
-            def qg = waitForQualityGate()
-            if (qg.status == 'IN_PROGRESS') {
-                error("⚠️ Quality Gate status 'IN_PROGRESS'. Aborting pipeline.")
-            } else if (qg.status != 'OK') {
-                error("❌ SonarQube Quality Gate failed with status: ${qg.status}")
-            } else {
-                echo "✅ SonarQube Quality Gate passed: ${qg.status}"
-            }
-        }
+//        sleep(30)
+//        script {
+//            def qg = waitForQualityGate()
+//            if (qg.status == 'IN_PROGRESS') {
+//                error("⚠️ Quality Gate status 'IN_PROGRESS'. Aborting pipeline.")
+//            } else if (qg.status != 'OK') {
+//                error("❌ SonarQube Quality Gate failed with status: ${qg.status}")
+//            } else {
+//                echo "✅ SonarQube Quality Gate passed: ${qg.status}"
+//            }
+//        }
     } catch (err) {
         error("❌ SonarQube analysis failed for project '${projectKey}': ${err}")
     }
