7ab58c97db4cff76eb09b32724d403c96adabf5e
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-05-07 16:21:18 +0200

    add enviroment

--- vars/sonarQubeAnalysisWithGate.groovy
+++ vars/sonarQubeAnalysisWithGate.groovy
@@ -18,7 +18,7 @@ def call(Map config = [:]) {
         echo "✅ SonarQube analysis completed for project '${projectKey}'."
 
         echo "⏳ Waiting Quality Gate..."
-        sleep(30)
+        sleep(10)
         script {
             def qg = waitForQualityGate()
             if (qg.status == 'IN_PROGRESS') {
