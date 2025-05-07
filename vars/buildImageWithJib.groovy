0c9071a7b2300884882517b7b9ead234c7038216
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-05-07 12:09:54 +0200

    add enviroment

--- vars/buildImageWithJib.groovy
+++ vars/buildImageWithJib.groovy
@@ -8,18 +8,9 @@ def call(Map config) {
                 fileId: config.mavenSettingsId,
                 variable: 'MAVEN_SETTINGS'
         )]) {
-            def IMAGE_JIB_VERSION = 'openjdk:17-jdk-slim'
-            def JIB_MAVEN_PLUGIN_VERSION = '3.4.5'
-            def REGISTRY_URL = 'nexus-registry.nexus.svc.cluster.local:64395/green-energy-docker-hosted'
             sh """
                     mvn com.google.cloud.tools:jib-maven-plugin:${JIB_MAVEN_PLUGIN_VERSION}:build \
-                    --settings \$MAVEN_SETTINGS \
-                    -Djib.from.image=${IMAGE_JIB_VERSION} \
-                    -Djib.to.image=${REGISTRY_URL}/${config.imageName}:${config.imageTag} \
-                    -Djib.to.auth.username=\$NEXUS_USERNAME \
-                    -Djib.to.auth.password=\$NEXUS_PASSWORD \
-                    -Djib.allowInsecureRegistries=true \
-                    -DsendCredentialsOverHttp=true
+                    --settings \$MAVEN_SETTINGS 
                 """
         }
     }
